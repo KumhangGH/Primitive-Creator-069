5513000cddbbbea2b455318c511f2a3847628eb9
Your Name <you@example.com>
2025-09-30 22:45:04 +0700

    add function in utill file

--- primUtil.py
+++ primUtil.py
@@ -1,1 +1,16 @@
-import maya.cmds as cmds
+created_obj = None
+
+		if obj_type == 'cube':
+			created_obj = cmds.polyCube(name=obj_name)[0]
+		elif obj_type == 'cone':
+			created_obj = cmds.polyCone(name=obj_name)[0]
+		elif obj_type == 'sphere':
+			created_obj = cmds.polySphere(name=obj_name)[0]
+		elif obj_type == 'torus':
+			created_obj = cmds.polyTorus(name=obj_name)[0]
+		else:
+			cmds.warning(f"Unsupported object type: {obj_type}")
+			return
+
+		cmds.select(created_obj)
+		print(f"Created object: {created_obj}")
